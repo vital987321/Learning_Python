1070bab81c9a6ba54e56bfc2d837c2aa55fe5b57
Vitalii Velychko <velychko@axl.local>
2023-12-08 16:57:48 +0100

    task 1 complete'

---
+++ Files_read_write/1_reverse_words.py
@@ -0,0 +1,18 @@
+# Task:
+# There is hw.txt file with some text.
+# Output all words from the file in reverse order and remove punctuations.
+
+text= ''
+try:
+    file =open('hw1.txt')
+    text=file.read()
+    file.close()
+except:
+    print ('file not found')
+
+if text:
+    text.strip()  # remove beginning/end spacers
+    punctuation='.,-*!?:;'
+    text=''.join(symbol for symbol in text if not symbol in punctuation) # removing punctuations and line feeds
+    text=' '.join(word for word in text.split()[::-1] )
+    print(text)
---
+++ Files_read_write/2_remove_row_number.py
@@ -0,0 +1,4 @@
+# Task:
+# There is hw.txt file with some text.
+# User input a row number. Remove this row from the text and save the text to seperate file.
+

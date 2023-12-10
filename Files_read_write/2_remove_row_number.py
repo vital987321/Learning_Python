80c500acf064d9b33c4bba4518ea5f2b4b436323
vital987321 <vitaliyvelichko@gmail.com>
2023-12-11 00:30:55 +0100

    Files_read_write task 2 is complete

--- Files_read_write/2_remove_row_number.py
+++ Files_read_write/2_remove_row_number.py
@@ -1,4 +1,20 @@
 # Task:
 # There is hw.txt file with some text.
-# User input a row number. Remove this row from the text and save the text to seperate file.
+# User inputs a row number. Remove this row from the text and save the text to separate file.
 
+with open('hw1.txt') as input_file:
+    lines=input_file.readlines()
+while True:
+    try:
+        user_row=int(input('Enter row number: '))
+        break
+    except:
+        print('This is not a number.')
+
+print(lines)
+lines.pop(user_row-1)
+print(lines)
+
+with open('2_remove_row_number_result.txt', 'w') as output_file:
+    for row in lines:
+        output_file.write(row)

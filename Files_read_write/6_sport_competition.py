2ad65076177a4e19f01b1ea3db75f37c61ab4124
Vitalii Velychko <velychko@axl.local>
2023-12-12 11:13:54 +0100

    Files_read_write task 6 initialized

---
+++ Files_read_write/6_sport_competition.py
@@ -0,0 +1,14 @@
+# Task 6
+# File 6_first_tour.txt contains a number K and a list of sportsmen with their score after first round/ Format is the following:
+#   80
+#   Zuckerberg Mark 80
+#   Musk Ilon 92
+#   Gates Bill 98
+#   Jobs Steve 78
+#
+# Filter the list of sportsmen for the second round. Criteria is a score must be greater than K.
+# Write result to file 6_second_tour.txt in the following format:
+#   2
+#   1)	B. Gates 98
+#   2)	I. Musk 92
+# The first number shows the number of participants.
